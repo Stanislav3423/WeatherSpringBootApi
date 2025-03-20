aad3d68dfd75e91d053a13704da564cf6c8a4a79
Stanislav3423 <svolk9954@gmail.com>
2025-03-20 16:05:10 +0200

    Fix Config Problem

---
+++ src/main/java/com/example/WeatherDataService/WebConfig.java
@@ -0,0 +1,24 @@
+package com.example.WeatherDataService;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:3000", "https://weatherspringbootapi-aganc0dbc2hub4cg.polandcentral-01.azurewebsites.net")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
