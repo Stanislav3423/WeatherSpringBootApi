ef60ed1c22e8e15c906dd5bd3412549377613878
Stanislav3423 <svolk9954@gmail.com>
2025-03-20 16:13:04 +0200

    Fix Bean problem

--- src/main/java/com/example/WeatherDataService/services/WebConfig.java
+++ src/main/java/com/example/WeatherDataService/services/WebConfig.java
@@ -1,16 +1,18 @@
 package com.example.WeatherDataService.services;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Вказуємо, що CORS налаштовується для всіх шляхів, що починаються з /api/
-                .allowedOrigins("http://localhost:3000") // Дозволяємо запити з порту 3000 (React)
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Дозволяємо GET, POST, PUT та DELETE запити
-                .allowedHeaders("*"); // Дозволяємо всі заголовки
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000", "https://weatherspringbootapi-aganc0dbc2hub4cg.polandcentral-01.azurewebsites.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
